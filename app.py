36a8cdc518f3d215f3551ac2b8e8d9262725fb8f
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-07-11 19:34:34 +0530

    app.py

--- app.py
+++ app.py
@@ -291,7 +291,7 @@ def login():
     with col7:
         st.markdown("")
         st.markdown("")
-        st.write("**                    Design & Developed by:**")
+        st.write("                             **Design & Developed by:**")
     with col9:
         st.image("https://lh3.googleusercontent.com/drive-viewer/AKGpiha2TPFebW5deI-uoKPuD2Yoq_4xws137dj4LaFW3APyb_BkQ5NNKtwtH__KDr3E-_KbygMPh3D5VPqZPR5-ymj17acBxJgBLA=s2560", width=150)
     with col8:

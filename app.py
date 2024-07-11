64869e4e7a75ecc951e2efd5aa6b8e5aa06faeb2
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-07-11 19:31:29 +0530

    app.py

--- app.py
+++ app.py
@@ -288,11 +288,11 @@ def login():
     col4, col5, col6, col7, col8, col9 = st.columns([1, 1, 1, 1, 1, 1])
     st.markdown("")
     st.markdown("")
-    with col4:
-        st.write("**Design & Developed by:**")
-    with col5:
-        st.image("https://lh3.googleusercontent.com/drive-viewer/AKGpiha2TPFebW5deI-uoKPuD2Yoq_4xws137dj4LaFW3APyb_BkQ5NNKtwtH__KDr3E-_KbygMPh3D5VPqZPR5-ymj17acBxJgBLA=s2560", width=150)
     with col7:
+        st.write("**Design & Developed by:**")
+    with col9:
+        st.image("https://lh3.googleusercontent.com/drive-viewer/AKGpiha2TPFebW5deI-uoKPuD2Yoq_4xws137dj4LaFW3APyb_BkQ5NNKtwtH__KDr3E-_KbygMPh3D5VPqZPR5-ymj17acBxJgBLA=s2560", width=150)
+    with col8:
         st.image("https://lh3.googleusercontent.com/drive-viewer/AKGpihZfq-IzkSSrziXqoQ8r0ypLiLPAQPW245Aq-NP6-90LEExUcBRM0L_mrr30zFXUzzN985zDpKWrcmptsWMF98vGmZjnfeEibg=s2560", width=150)
 
 
